fedc99eb11db0f1d921dee1053fc48c918f49fad
John <johntony366@gmail.com>
2022-09-19 00:21:28 +0530

    feat(startPage): Finish UI for starting page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import "./App.css";
+import { Box } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import StartPage from "./components/StartPage";
+import blueBlob from "./assets/blueBlob.svg";
+import yellowBlob from "./assets/yellowBlob.svg";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Karla", "sans-serif"].join(","),
+  },
+  palette: {
+    primary: {
+      main: "#4D5B9E"
+    }
+  }
+});
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Box
+        className="App"
+        sx={{
+          backgroundColor: "#F5F7FB",
+          backgroundImage: `url(${blueBlob}), url(${yellowBlob})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "bottom left, top right",
+        }}
+      >
+        <StartPage />
+      </Box>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/StartPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+
+export default function StartPage() {
+  return (
+    <Box
+      className="startPage"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      minHeight={"100vh"}
+      gap={4}
+    >
+      <Typography variant="h1" fontWeight={700} color="primary.dark">Quizzlet</Typography>
+      <Button
+        variant="contained"
+        sx={{ textTransform: "capitalize", fontSize: "24px", px: 8, borderRadius: 6 }}
+      >
+        Start
+      </Button>
+    </Box>
+  );
+}
